a498a638b79a584c8f2fdea9df5c9c4593a81c09
xitij27 <kp27social@gmail.com>
2025-09-26 20:04:51 +0530

    implemented wiping old data

--- app/routes.ts
+++ app/routes.ts
@@ -5,5 +5,6 @@ export default [
     route('/auth', 'routes/auth.tsx'),
     route('/upload', 'routes/upload.tsx'),
     route('/resume/:id', 'routes/resume.tsx'),
+    route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
 
