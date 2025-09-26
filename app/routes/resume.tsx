e98271ce08bb43cc6cabd0e039f3d71b1dfb195b
xitij27 <kp27social@gmail.com>
2025-09-26 19:18:33 +0530

    [feat] Resume feedback components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,9 @@
 import {Link, useNavigate, useParams} from "react-router";
 import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
+import Summary from "~/components/Summary";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
 
 export const meta = () => ([
     {title: 'Resumind | Review'},
